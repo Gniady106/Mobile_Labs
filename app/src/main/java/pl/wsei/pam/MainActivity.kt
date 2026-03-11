a7d510c96f8402b6143ab375e6f2e7d83649df89
Michał Gniadek <michal.gniadek@microsoft.wsei.edu.pl>
2026-03-11 13:32:10 +0100

    messages and icons done

--- app/src/main/java/pl/wsei/pam/MainActivity.kt
+++ app/src/main/java/pl/wsei/pam/MainActivity.kt
@@ -1,5 +1,6 @@
-package pl.wsei.pam.lab02
+package pl.wsei.pam
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -8,28 +9,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pl.wsei.pam.lab01.R
+import pl.wsei.pam.lab01.Lab01Activity
+import pl.wsei.pam.lab02.Lab02Activity
 
-class Lab02Activity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_lab02)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.favorites_grid)) { v, insets ->
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-
-
-
-
     }
-    fun selectBoard(v: View){
-        val tag: String? = v.tag as String?
-        val tokens: List<String>? = tag?.split(" ")
-        val rows = tokens?.get(0)?.toInt()
-        val columns = tokens?.get(1)?.toInt()
-        Toast.makeText(this, "rows: ${rows}, columns: ${columns}", Toast.LENGTH_SHORT).show()
+
+    fun onClickMainBtnRunLab01(v: View){
+        val intent = Intent(this, Lab01Activity::class.java)
+        startActivity(intent)
+    }
+    fun onClickMainBtnRunLab02(v: View){
+        val intent = Intent(this, Lab02Activity::class.java)
+        startActivity(intent)
     }
 }
